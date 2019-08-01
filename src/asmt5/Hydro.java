5568ac77abd8f2bd9d6c7934ebded1d1dedc78f7
navpr <navpr@192.168.0.13>
2019-07-31 20:03:32 -0400

    getter/setters

--- src/asmt5/Hydro.java
+++ src/asmt5/Hydro.java
@@ -1,8 +1,35 @@
 package asmt5;
 
+import java.util.Date;
+
 //hydro class extends bill
 	class Hydro extends Bill
 	{
-	String agency_name;
-	String unit_consume;
+		String agency_name;
+		String unit_consume;
+		//auto generated constrcutor
+	public Hydro(int bill_id, Date bill_date, String bill_type, double tbill_amount) {
+			super(bill_id, bill_date, bill_type, tbill_amount);
+			// TODO Auto-generated constructor stub
+	//constructor properties
+			this.agency_name=agency_name;
+			this.unit_consume=unit_consume;
+	}
+	//getters
+	public String getAgency_name() {
+		return agency_name;
+	}
+	public String getUnit_consume() {
+		return unit_consume;
+	}
+	//setters
+	public void setAgency_name(String agency_name) {
+		this.agency_name = agency_name;
+	}
+	public void setUnit_consume(String unit_consume) {
+		this.unit_consume = unit_consume;
+	}
+	
+	
+	
 	}
